c3a7dcdfd5efa4e5f4e8d4b5dd1abe803e982967
jagveers <jsingh@ex2india.com>
2023-02-23 00:49:54 +0530

    added file

--- jobs/demo_etl_job.py
+++ jobs/demo_etl_job.py
@@ -1,14 +1,12 @@
 import sys
-from awsglue.transforms import *
-from awsglue.utils import getResolvedOptions
-import pyspark
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
+from awsglue.transforms import *
 from awsglue.dynamicframe import DynamicFrame
 from awsglue.utils import getResolvedOptions
-from awsglue.transforms import *
 from awsglue.job import Job
-from pyspark.sql.functions import col, avg
+import pyspark
+
 
 #### ###creating spark and gluecontext ###############
 def transform_and_save(database_name, table_name):
@@ -22,22 +20,12 @@ def transform_and_save(database_name, table_name):
     ## ###Creating glue dynamic frame from the catalog ###########
     input_dyf = gluecontext.create_dynamic_frame.from_catalog(database = database_name, table_name = table_name)
 
-    ## convert the DynamicFrame to a DataFrame
-    input_df = input_dyf.toDF()
-
-    ## Transforming the data
-    transformed_df = input_df.filter(input_df.suppressed.isNull()) \
-                            .filter(input_df.period.like("2011%")) \
-                            .selectExpr("series_reference", "period", "CAST(data_value.long AS DOUBLE) AS data_value", "units") \
-                            .groupBy("period") \
-                            .agg(avg("data_value").alias("avg_data_value"))
-
-    ## Converting the transformed DataFrame back to a DynamicFrame
-    transformed_dyf = DynamicFrame.fromDF(transformed_df, gluecontext, "transformed_dyf")
-
-    ## Writing the transformed data to the S3 bucket
     s3_output_path = "s3://glue-serverless-demo/output-data/"
-    gluecontext.write_dynamic_frame.from_options(frame = transformed_dyf, connection_type = "s3", connection_options = {"path": s3_output_path}, format = "csv")
+    gluecontext.write_dynamic_frame.from_options(\
+            frame = input_dyf, \
+            connection_type = "s3", \
+            connection_options = {"path": s3_output_path \
+                }, format = "parquet")  
 
     job.commit()
 
